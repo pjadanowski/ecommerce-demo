1ce63c13ffba40e48a43e5b549281788fbe5426e
Paweł Jadanowski <pjadanowski@gmail.com>
2022-04-15 13:49:15 +0200

    Create PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
     public function index()
     {
         $products = Product::limit(4)->get();
-        // dd($products->first()->price);
+
         return view('pages.index', compact('products'));
     }
 }
